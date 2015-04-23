090b07ce9db6b23fa06cbda21c2d0c2f3be0f596
Mauricio Gamboa <Mauricio Gamboa@172.26.107.100>
2015-04-23 13:40:21 -0600

    Añadí modificación Cola

--- src/mensajeria/Cola.java
+++ src/mensajeria/Cola.java
@@ -63,30 +63,43 @@ public class Cola {
         }
     } 
     
-        public Mensaje devolver_mayor_prioridad_explícito(String proceso){ //Mayor Prioridad Fifo Explícito
+      public Mensaje devolver_mayor_prioridad_explícito(String proceso){ //Mayor Prioridad Fifo Explícito
         List<Mensaje> ListaMSN= this.lista; 
         int tamano_lista= ListaMSN.size();
         System.out.println("Tamaño");
         System.out.println(ListaMSN.size());
-        int contador=0;
+        int contador=1;
         Mensaje res;
-        res= null;
+        res= ListaMSN.get(0);
         if (tamano_lista>1){
         while (contador<tamano_lista){
-            if (ListaMSN.get(contador).prioridad<res.prioridad){
+            if ((ListaMSN.get(contador).prioridad<res.prioridad)&&(ListaMSN.get(contador).origen.nombre.equals(proceso))){
                 res=ListaMSN.get(contador);
             }
             else{
                 contador++;
             }
         }
-        System.out.println(res.contenido);
-        return res;
+        
+        if (res.origen.nombre.equals(proceso)){
+            System.out.println(res.contenido);
+            return res; 
+        }
+        else{
+            
+            return null; 
+        }
         }
         else{
             if (tamano_lista==1){
+                
+                if (res.origen.nombre.equals(proceso)){
                 System.out.println(res.contenido);
                 return res; 
+                }
+                else{
+                    return null; 
+                }
                 
             }
             else{
@@ -101,73 +114,35 @@ public class Cola {
     
     
     public static void main(String[] args) {
+        Proceso p1 = new Proceso("Proceso1"); 
+        Proceso p2 = new Proceso("Proceso2"); 
+        Proceso p3 = new Proceso("Proceso3"); 
+        
         List <Mensaje> ListaMensajes = new LinkedList<Mensaje>(); 
-        Mensaje ms1 = new Mensaje(1,3,"Mensaje hola"); 
-        Mensaje ms2 = new Mensaje(1,2,"Mensaje hola2");
-        Mensaje ms3 = new Mensaje(1,2,"Mensaje hola3");
-        Mensaje ms4 = new Mensaje(1,3,"Mensaje hola4");
-        Mensaje ms5 = new Mensaje(2,2,"Mensaje hola5");
-        Mensaje ms6 = new Mensaje(2,1,"Mensaje hola6");
-        ListaMensajes.add(ms1); 
-        ListaMensajes.add(ms2);
-        ListaMensajes.add(ms3);
-        ListaMensajes.add(ms4);
+        Mensaje ms1 = new Mensaje(p1,p2,"Mensaje hola",1); 
+        Mensaje ms2 = new Mensaje(p1,p3,"Mensaje hola2",2);
+        Mensaje ms3 = new Mensaje(p3,p2,"Mensaje hola3",1);
+        Mensaje ms4 = new Mensaje(p2,p1,"Mensaje hola4",3);
+        Mensaje ms5 = new Mensaje(p2,p3,"Mensaje hola5",2);
+        Mensaje ms6 = new Mensaje(p3,p1,"Mensaje hola6",2);
+   
         Cola x = new Cola(ListaMensajes);
         x.agregar_final(ms1);
+        x.agregar_final(ms2);
+        x.agregar_final(ms3);
+        x.agregar_final(ms4);
         x.agregar_final(ms5);
         x.agregar_final(ms6);
-        System.out.println("Tamañoooooooooooo");
-        System.out.println(x.tamano());
-        System.out.println("Tamañoooooooooooo");
-        System.out.println(ListaMensajes.size());
-        System.out.println(ListaMensajes.get(0).contenido);
+        
+        //System.out.println(ListaMensajes.get(0).contenido);
         //ListaMensajes.remove(0);
-        System.out.println(ListaMensajes.get(0).contenido);
+        //System.out.println(ListaMensajes.get(0).contenido);
         
         System.out.println("Mayor Prioridad");
         
         x.devolver_mayor_prioridad(); 
-        
-        /*   
-        //Inicia
-        // Devolver el nodo de mayor prioridad
-        int tamano_lista= ListaMensajes.size();
-        System.out.println("Tamaño");
-        System.out.println(ListaMensajes.size());
-        int contador=1;
-        Mensaje res;
-        res= ListaMensajes.get(0);
-        if (tamano_lista>1){
-        while (contador<tamano_lista){
-            if (ListaMensajes.get(contador).prioridad<res.prioridad){
-                res=ListaMensajes.get(contador);
-            }
-            else{
-                contador++;
-            }
-        }
-        //return res;
-        System.out.println(res.contenido);
-        }
-        else{
-            if (tamano_lista==1){
-                //return
-                System.out.println(res.contenido);
-                
-            }
-            else{
-                System.out.println("error");
-            }
-        }
-        
-        
-        //Finaliza 
-        */
-        
-        //Inicia
-        
-        
-        //Finaliza
+        System.out.println("Mayor Prioridad Explícito FIFO");
+        x.devolver_mayor_prioridad_explícito("Proceso1"); 
     }
     
 }
