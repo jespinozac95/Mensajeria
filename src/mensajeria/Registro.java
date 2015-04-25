d49e8d191d106134d076a1b4e7112baf79023de0
Mauricio Gamboa <Mauricio Gamboa@172.26.107.229>
2015-04-25 15:39:58 -0600

    Agregué Registro

---
+++ src/mensajeria/Registro.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mensajeria;
+
+/**
+ *
+ * @author Mauricio Gamboa
+ */
+public class Registro {
+    String fecha=null;
+    String accion; 
+    String origen; 
+    String estado_origen; 
+    String destino; 
+    String mensaje; 
+    
+    
+    public Registro(String fecha, String accion,String origen, String estado, String destino, String mensaje){
+        this.fecha=fecha; 
+        this.accion=accion; 
+        this.origen=origen; 
+        this.estado_origen=estado; 
+        this.destino=destino; 
+        this.mensaje=mensaje; 
+        
+    }
+    
+    public String fecha_actual(){
+        java.util.Date utilDate = new java.util.Date();
+        long lnMilisegundos = utilDate.getTime();
+        java.sql.Timestamp sqlTimestamp = new java.sql.Timestamp(lnMilisegundos);
+        utilDate = sqlTimestamp;
+        return utilDate.toString(); 
+    }
+}
